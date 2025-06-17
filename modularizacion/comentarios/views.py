b89aef3439caefaff81089867b5dcdd5e21bc814
Rocha <marianorocha03@gmail.com>
2025-06-16 20:05:44 -0600

    Subimos el proyecto de modularizacion

--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Comment
 
-# Create your views here.
+def test(request):
+    return HttpResponse("Funciona correctamente")
+
+def create(response):
+    # comment = Comment(name="Juanjo", score=5, comment="Este es un comentario")
+    # comment.save()
+    comment = Comment.objects.create(name='Alex')
+    return HttpResponse("Ruta para comprobar evaluacion")
+
+def delete(request):
+    #comment = Comment.objects.get(id=1)
+    #comment.delete()
+    Comment.objects.filter(id=2).delete()
+    return HttpResponse("Ruta para probar los borrados")
